877fdbd396293686af5e27f2c601b292473f380d
Hai Shu <shuhai.edu@gmail.com>
2021-08-02 23:54:42 -0400

    Update example.py

--- example.py
+++ example.py
@@ -8,10 +8,10 @@ Yk = [[] for _ in range(n_sets)]
 for i in range(n_sets):
     Yk[i] = np.load('Y' + str(i) + '.npy')
  
-#Select the ranks of the Xk matrices by the ED method
+# Select the ranks of the Xk matrices by the ED method
 Xk_hat, Ck_hat, Dk_hat, PVE_by_Ck_SetLevel_hat,  PVE_by_Ck_VariableLevel_hat  = dgcca.dGCCA(Yk, method='ED', sig_level=0.05, bootstrap_samples=10000)
 
-# Use user-specified ranks of the Xk matrices (e.g., from the scree plot) 
+# Or use user-specified ranks of the Xk matrices (e.g., from the scree plot) 
 # Xk_hat, Ck_hat, Dk_hat, PVE_by_Ck_SetLevel_hat,  PVE_by_Ck_VariableLevel_hat  = dgcca.dGCCA(Yk, rankX=[5,5,5], sig_level=0.05, bootstrap_samples=10000)
 
                    
